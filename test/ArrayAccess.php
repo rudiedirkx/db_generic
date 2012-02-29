a47c5a5dda4129dbd26bb52538f5ddf2e5835f3c
Rudie Dirkx <github@hotblocks.nl>
2012-02-29 03:21:51 +0100

    ArrayAccess tests+

--- test/ArrayAccess.php
+++ test/ArrayAccess.php
@@ -14,9 +14,20 @@ print_r($stuff);
 echo "\n";
 
 var_dump($stuff->id);
-//~ var_dump($stuff->xid); // Notice ... in ArrayAccess.php
+//~ var_dump($stuff->xid); // Notice ... in __FILE__
 var_dump($stuff['stuff']);
 //~ var_dump($stuff['xstuff']); // Notice ... in db_generic.php =(
+
+
+echo "\n\n\n";
+
+
+echo "without ArrayAccess:\n";
+db_generic_result::$return_object_class = 'stdClass';
+
+$stuff = $db->select('stuffs', 'stuff <> ? ORDER BY RAND() LIMIT 1', array('Q'), true);
+var_dump($stuff->id);
+//var_dump($stuff['stuff']); // Fatal error ... in __FILE__
 
 
 
